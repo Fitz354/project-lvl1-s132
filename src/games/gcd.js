cbe37ae94519ae7811b88a5978e991ff9b3f2f8e
Fitz354 <lozovsky.alx@yandex.ru>
2017-10-13 11:19:48 +0300

    rename getMaxDivider gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,18 +4,18 @@ import run from '../game';
 
 const maxValue = 100;
 const rulesMessage = 'Find the greatest common divisor of given numbers.';
-const getMaxDivider = (a, b) => {
+const getMaxDivisor = (a, b) => {
   if (b === 0) {
     return a;
   }
 
-  return getMaxDivider(b, a % b);
+  return getMaxDivisor(b, a % b);
 };
 
 const createTask = () => {
   const num1 = getRandomNumber(maxValue);
   const num2 = getRandomNumber(maxValue);
-  const answer = getMaxDivider(num1, num2);
+  const answer = getMaxDivisor(num1, num2);
 
   return cons(`${num1} ${num2}`, String(answer));
 };
